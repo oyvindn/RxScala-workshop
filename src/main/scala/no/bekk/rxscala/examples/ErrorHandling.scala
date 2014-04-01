c188a2dae1acd7d8499edffdb804efffaf414750
Øyvind Nerbråten <oyvind@nerbraten.no>
2014-04-01 21:41:37 +0200

    Added error handling example

---
+++ src/main/scala/no/bekk/rxscala/examples/ErrorHandling.scala
@@ -0,0 +1,28 @@
+package no.bekk.rxscala.examples
+
+import rx.lang.scala.Observable
+import no.bekk.rxscala.util.ThreadRunner
+
+object ErrorHandling extends App {
+  def customNonBlockingObservableWithErrorHandling(): Observable[String] = {
+    Observable(subscriber => {
+      // For simplicity this example uses a Thread instead of an ExecutorService/ThreadPool
+      ThreadRunner {
+        0 to 50 foreach (n => {
+          if (!subscriber.isUnsubscribed) {
+            if(n < 30) {
+              subscriber.onNext(s"value $n")
+            } else {
+              subscriber.onError(new RuntimeException("I am tired, do it yourself!"))
+            }
+          }
+        })
+
+        // after sending all values we complete the sequence
+        subscriber.onCompleted()
+      }
+    })
+  }
+
+  customNonBlockingObservableWithErrorHandling().subscribe(next => println(next), error => println(s"error: ${error.getMessage}"))
+}
