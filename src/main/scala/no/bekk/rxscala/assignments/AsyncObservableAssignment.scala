91078897f57fe1a4d8d74e0df638003bbe17b5ea
Øyvind Nerbråten <oyvind.nerbraten@bekk.no>
2014-04-05 10:38:50 +0200

    Made the reference implementation a bit more readable

--- src/main/scala/no/bekk/rxscala/assignments/AsyncObservableAssignment.scala
+++ src/main/scala/no/bekk/rxscala/assignments/AsyncObservableAssignment.scala
@@ -11,7 +11,10 @@ object AsyncObservableAssignment {
   // Create an Observable that emits the result of the two calculations.
   def mathCalculationsObservable(mathCalculation1: Calculation, mathCalculation2: Calculation): Observable[Int] = {
     def toAsyncObservable(calculation: Calculation) = Observable.from(future(calculation()))
-    toAsyncObservable(mathCalculation1).merge(toAsyncObservable(mathCalculation2))
+
+    val obsCalc1 = toAsyncObservable(mathCalculation1)
+    val obsCalc2 = toAsyncObservable(mathCalculation2)
+    obsCalc1.merge(obsCalc2)
   }
 
 }
