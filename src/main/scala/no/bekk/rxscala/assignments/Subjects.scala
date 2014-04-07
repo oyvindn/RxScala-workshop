f57c7e43e2e21a1bdedf849245200a58e2fbd2a1
Håkon Haga <hakon.haga@bekk.no>
2014-04-07 20:52:11 +0200

    subject example with sums

--- src/main/scala/no/bekk/rxscala/assignments/Subjects.scala
+++ src/main/scala/no/bekk/rxscala/assignments/Subjects.scala
@@ -1,10 +1,15 @@
 package no.bekk.rxscala.assignments
 
-import rx.lang.scala.subjects.{ReplaySubject, AsyncSubject}
-import rx.lang.scala.{Subject, Observable}
+import rx.lang.scala.{Subscription, Subject, Observable}
 
 
 object Subjects {
+
+  var total = 0
+
+  def createSumSubscriber(subject: Subject[Int]): Subscription = {
+    subject.subscribe(n => total += n)
+  }
 
   def concatObservableAndSubject(obs: Observable[Int], repSub: Subject[Int]) = {
     obs ++ repSub
--- src/test/scala/no/bekk/rxscala/assignments/SubjectsSpec.scala
+++ src/test/scala/no/bekk/rxscala/assignments/SubjectsSpec.scala
@@ -3,27 +3,43 @@ package no.bekk.rxscala.assignments
 import org.scalatest._
 import org.scalatest.FlatSpec
 import scala.concurrent._
-import scala.util.{Success, Failure, Try}
+import scala.util.Try
 import ExecutionContext.Implicits.global
 import scala.concurrent.duration._
-import rx.lang.scala.{Subject, Observable}
-import rx.subjects.PublishSubject
+import rx.lang.scala._
+import rx.lang.scala.subjects.ReplaySubject
+import scala.util.Success
+import scala.util.Failure
 
 class SubjectsSpec extends FlatSpec with Matchers {
 
+  it should "create subsribers that sums up each value on subject stream" in {
+    val subject = Subject[Int]()
+
+    val a = Subjects.createSumSubscriber(subject)
+    val b = Subjects.createSumSubscriber(subject)
+
+    subject.onNext(1)
+    subject.onNext(2)
+
+    a.unsubscribe()
+    subject.onNext(3)
+
+    subject.onCompleted()
+
+    assert(Subjects.total === 1 + 1 + 2 + 2 + 3)
+
+  }
+
   it should "use subject to get more values on to an existing observable" in {
 
-    val subject = Subject[Int]() //TASK: Replace with correct subject type
+    val subject = ReplaySubject[Int]() //TASK: Replace with correct subject type
     val initial = Observable.from(List(1,2,3,4))
     val target = Subjects.concatObservableAndSubject(initial, subject)
 
     subject.onNext(5)
-
-    val subscription1 = target subscribe(List(_))
-
     subject.onNext(6)
     subject.onCompleted()
-    subscription1.unsubscribe()
     subject.onNext(7)
 
     val f = future(target.toBlockingObservable.toList)
