d0499079801c8963a509f296a3bb48c3f50722a6
Øyvind Nerbråten <oyvind@nerbraten.no>
2014-04-07 20:56:04 +0200

    Consolidated the basic assignments to one file

--- src/main/scala/no/bekk/rxscala/assignments/BasicObservables.scala
+++ src/main/scala/no/bekk/rxscala/assignments/BasicObservables.scala
@@ -1,6 +1,9 @@
 package no.bekk.rxscala.assignments
 
 import rx.lang.scala.Observable
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration._
+import ExecutionContext.Implicits.global
 
 object BasicObservables {
 
@@ -15,11 +18,25 @@ object BasicObservables {
    * Create an observable that for each time there is a value from both lists, emits the sum of the who values
    */
   def observableSums(o1: Observable[Int], o2: Observable[Int]): Observable[Int] = o1.zip(o2).map { case (x,y) => x + y }
-  
-  
-  
+
+
+
   /*
    * Create an observable that emits the sum of the 10 last positive numbers emitted by the given observable
    */
   def observableSumLastTen(observable: Observable[Int]): Observable[Int] = observable.filter(_ > 0).takeRight(10).sum
+
+
+
+  /*
+   * Create an observable emitting the value produced by the Future as its single item.
+   */
+  def observableFuture(future: Future[List[String]]): Observable[List[String]] = Observable.from(future)
+
+
+
+  /*
+   * Create an observable using a timer or interval that emits a new Int every 100ms, and transform it to emit buffers of 5 values at a time
+   */
+  def observableTimer: Observable[Seq[Long]] = Observable.interval(100 millisecond).buffer(5)
 }
--- src/test/scala/no/bekk/rxscala/assignments/BasicObservablesSpec.scala
+++ src/test/scala/no/bekk/rxscala/assignments/BasicObservablesSpec.scala
@@ -1,8 +1,11 @@
 package no.bekk.rxscala.assignments
 
 import org.scalatest.{Matchers, FlatSpec}
-import scala.util.Random
+import scala.util.{Success, Failure, Try, Random}
 import rx.lang.scala.Observable
+import scala.concurrent._
+import scala.concurrent.duration._
+import ExecutionContext.Implicits.global
 
 class BasicObservablesSpec extends FlatSpec with Matchers {
 
@@ -25,6 +28,28 @@ class BasicObservablesSpec extends FlatSpec with Matchers {
     val list = randomList(100)
     val result = BasicObservables.observableSumLastTen(Observable.from(list)).toBlockingObservable.single
     assert(result === list.filter(_ > 0).takeRight(10).sum)
+  }
+
+  it should "create an observable emitting the value produced by the Future as its single item" in {
+    val f: Future[List[String]] = future {
+      List("Camping", "filming", "test")
+    }
+
+    val result = BasicObservables.observableFuture(f).toBlockingObservable.toList
+    assert(result.size === 1)
+    assert(result.head === List("Camping", "filming", "test"))
+  }
+
+  it should "create an observable with a timer that emits a new int every 100ms and buffers it in blocks of 5" in {
+    val observable = BasicObservables.observableTimer.debounce(100 millisecond).take(3)
+
+    val f = future(observable.toBlockingObservable.toList)
+    Try(Await.result(f, 1600 millisecond)) match {
+      case Failure(e) => fail("We don't have all day!")
+      case Success(result) => {
+        result should contain theSameElementsInOrderAs (List(List(0,1,2,3,4), List(5,6,7,8,9), List(10,11,12,13,14)))
+      }
+    }
   }
 
   private def randomList(n: Int) = Seq.fill(n)(Random.nextInt).toList
