33f880d01c04769fd0bbfd244825323220e970d6
Øyvind Nerbråten <oyvind@nerbraten.no>
2014-04-07 18:44:39 +0200

    More basic observable assignments

--- src/main/scala/no/bekk/rxscala/assignments/BasicObservables.scala
+++ src/main/scala/no/bekk/rxscala/assignments/BasicObservables.scala
@@ -9,4 +9,17 @@ object BasicObservables {
    */
   def observableFromList(list: List[Int]): Observable[Int] = Observable.from(list).map(_ * 2)
 
+
+
+  /*
+   * Create an observable that for each time there is a value from both lists, emits the sum of the who values
+   */
+  def observableSums(o1: Observable[Int], o2: Observable[Int]): Observable[Int] = o1.zip(o2).map { case (x,y) => x + y }
+  
+  
+  
+  /*
+   * Create an observable that emits the sum of the 10 last positive numbers emitted by the given observable
+   */
+  def observableSumLastTen(observable: Observable[Int]): Observable[Int] = observable.filter(_ > 0).takeRight(10).sum
 }
--- src/test/scala/no/bekk/rxscala/assignments/BasicObservablesSpec.scala
+++ src/test/scala/no/bekk/rxscala/assignments/BasicObservablesSpec.scala
@@ -2,14 +2,31 @@ package no.bekk.rxscala.assignments
 
 import org.scalatest.{Matchers, FlatSpec}
 import scala.util.Random
+import rx.lang.scala.Observable
 
 class BasicObservablesSpec extends FlatSpec with Matchers {
 
   it should "create an observable that emits each of the elements in the list multiplied with 2" in {
-    val list = Seq.fill(100)(Random.nextInt).toList
+    val list = randomList(100)
     val result = BasicObservables.observableFromList(list).toBlockingObservable.toList
     assert(result.size === list.size)
-    result should contain theSameElementsAs (list.map(_ * 2))
+    result should contain theSameElementsInOrderAs (list.map(_ * 2))
   }
+
+  it should "create an observable that for each time there is a value from both lists, emits the sum of the who values" in {
+    val list1 = randomList(100)
+    val list2 = randomList(100)
+    val result = BasicObservables.observableSums(Observable.from(list1), Observable.from(list2)).toBlockingObservable.toList
+    assert(result.size === list1.size)
+    result should contain theSameElementsInOrderAs (list1.zip(list2).map { case (x,y) => x + y})
+  }
+
+  it should "create an observable that emits the sum of the 10 last positive numbers emitted by the given observable" in {
+    val list = randomList(100)
+    val result = BasicObservables.observableSumLastTen(Observable.from(list)).toBlockingObservable.single
+    assert(result === list.filter(_ > 0).takeRight(10).sum)
+  }
+
+  private def randomList(n: Int) = Seq.fill(n)(Random.nextInt).toList
 
 }
