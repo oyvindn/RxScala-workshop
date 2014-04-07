0efbb4b9cb69915ec6521b18634ea9461a570219
Øyvind Nerbråten <oyvind@nerbraten.no>
2014-04-07 22:38:49 +0200

    Removed the assignment implementations

--- src/main/scala/no/bekk/rxscala/assignments/BasicObservables.scala
+++ src/main/scala/no/bekk/rxscala/assignments/BasicObservables.scala
@@ -7,20 +7,20 @@ object BasicObservables {
   /*
    * Create an observable that emits each of the elements in the list multiplied with 2
    */
-  def observableFromList(list: List[Int]): Observable[Int] = Observable.from(list).map(_ * 2)
+  def observableFromList(list: List[Int]): Observable[Int] = ???
 
 
 
   /*
    * Create an observable that for each time there is a value from both lists, emits the sum of the who values
    */
-  def observableSums(o1: Observable[Int], o2: Observable[Int]): Observable[Int] = o1.zip(o2).map { case (x,y) => x + y }
+  def observableSums(o1: Observable[Int], o2: Observable[Int]): Observable[Int] = ???
 
 
 
   /*
    * Create an observable that emits the sum of the 10 last positive numbers emitted by the given observable
    */
-  def observableSumLastTen(observable: Observable[Int]): Observable[Int] = observable.filter(_ > 0).takeRight(10).sum
+  def observableSumLastTen(observable: Observable[Int]): Observable[Int] = ???
 
 }
